3784b38ab28b98864e98620abf31cf4567c78f3b
YohanV <vienne.yohan@gmail.com>
2017-11-19 01:31:31 +0100

    Ajout des personnages

--- game.py
+++ game.py
@@ -5,7 +5,7 @@ import Fonction
 
 #Déclaration de la longueur de la fenetre
 SCREEN_WIDTH=640
-SCREEN_HEIGHT=480
+SCREEN_HEIGHT=470
 
 #Declaration des variables
 game_running = True
@@ -16,8 +16,15 @@ screen_game = Fonction.screen(SCREEN_WIDTH, SCREEN_HEIGHT)
 #Chargment de la texture de fond
 Fonction.background(screen_game)
 
+#Affichage du joueur
+Fonction.player(screen_game)
+Fonction.gardien(screen_game)
 pygame.display.flip()
 
 while game_running:
-	game_running=int(input())
-	continue
+	for event in pygame.event.get():
+		#pygame prend le premier évènement de la file
+		if event.type==QUIT:
+			#l'évènement QUIT correspond au clic sur la croix
+			game_running = 0
+			#permet de quitter la boucle 
