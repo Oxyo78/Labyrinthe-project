90dd2f173aa5b2b6d46ebe97158b4b327df4aabc
Oxyo78 <vienne.yohan@gmail.com>
2017-12-02 10:22:55 +0100

    maj

--- build/exe.win-amd64-3.6/config.py
+++
@@ -1,7 +0,0 @@
-#coding:utf-8
-#! /usr/bin/env python3
-
-""" Configuration of the game"""
-
-# Set size of the sprite in pixel
-sprite_size = 30
--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
-"""Fichier d'installation de notre script salut.py."""
+"""Setup file to create a exe"""
 
 from cx_Freeze import setup, Executable
 
-# On appelle la fonction setup
+# call setup  and add files / folder
 setup(
     name = "Save MacGyver !",
     version = "1.00",
-    options= {"build_exe": {"packages": ["pygame"]}},
+    options= {"build_exe": {"packages": ["pygame"],
+                            "include_files": ["class_game.py", "map", "picture", "fonction"]}},		
     description = "OC-Project 3 by Yohan V",
     executables = [Executable("main.py")],
 )
